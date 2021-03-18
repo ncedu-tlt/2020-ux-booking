3194b1bb6cd34209542d6581508032b84e487864
Alexander Ionov <orlicus@gmail.com>
2021-03-18 22:16:50 +0400

    Fixed storybook

--- apps/frontend/src/app/common/button/button.component.stories.ts
+++ apps/frontend/src/app/common/button/button.component.stories.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { ButtonComponent } from './button.component';
+import { Meta, Story } from '@storybook/angular/types-6-0';
+import { StoryFnAngularReturnType } from '@storybook/angular/dist/client/preview/types';
 
-export default {
-    title: 'Button',
+const meta: Meta = {
+    title: 'Button'
+}
+export default meta;
+
+const common: StoryFnAngularReturnType = {
     moduleMetadata: {
-        imports: [BrowserModule]
-    },
-    component: ButtonComponent
+        declarations: [ButtonComponent]
+    }
 }
 
-export const primary = () => ({
-    props: {
-        type: 'primary'
-    }
+export const regular: Story<ButtonComponent> = () => ({
+    ...common,
+    template: '<b-button>Test</b-button>'
+});
+
+export const primary: Story<ButtonComponent> = () => ({
+    ...common,
+    template: '<b-button type="primary">Test</b-button>'
 });
