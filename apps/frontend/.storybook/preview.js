f37858135d9949c24f3a04f65fa1c0798ed868e7
Alexander Ionov <orlicus@gmail.com>
2021-03-24 21:38:07 +0400

    Use global styles in Storybook

--- apps/frontend/.storybook/preview.js
+++ apps/frontend/.storybook/preview.js
@@ -1,4 +1,10 @@
 import { addDecorator } from '@storybook/angular';
 import { withKnobs } from '@storybook/addon-knobs';
 
+import '../src/styles.less';
+
+export const parameters = {
+  layout: 'centered'
+};
+
 addDecorator(withKnobs);
