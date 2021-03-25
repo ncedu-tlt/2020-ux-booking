6e273c0b3cb43fcb8e9de47f107faf1df9a9dea9
rich-alien <Richalien@yandex.ru>
2021-03-25 22:46:18 +0300

    add component

--- apps/frontend/src/app/common/common.module.ts
+++ apps/frontend/src/app/common/common.module.ts
@@ -9,6 +9,7 @@ import { HeaderComponent } from './header/header.component';
 import { I18NextModule } from 'angular-i18next';
 import { StarSelectorComponent } from './star-selector/star-selector.component';
 import { ButtonIconComponent } from './icon/button-icon.component';
+import { SelectorComponent } from './selector/selector.component';
 
 @NgModule({
   imports: [NgCommonModule, AngularSvgIconModule, I18NextModule],
@@ -19,7 +20,8 @@ import { ButtonIconComponent } from './icon/button-icon.component';
     DropdownComponent,
     HeaderComponent,
     StarSelectorComponent,
-    ButtonIconComponent
+    ButtonIconComponent,
+    SelectorComponent
   ],
   exports: [
     ButtonComponent,
@@ -28,7 +30,8 @@ import { ButtonIconComponent } from './icon/button-icon.component';
     DropdownComponent,
     HeaderComponent,
     StarSelectorComponent,
-    ButtonIconComponent
+    ButtonIconComponent,
+    SelectorComponent
   ]
 })
 export class CommonModule {}
