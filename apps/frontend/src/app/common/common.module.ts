a3233fe8d9e0671274abfa52eda80a891dbca2f9
rich-alien <Richalien@yandex.ru>
2021-03-25 22:46:18 +0300

    add component

--- apps/frontend/src/app/common/common.module.ts
+++ apps/frontend/src/app/common/common.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule as NgCommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { SelectorComponent } from './selector/selector.component';
 
 @NgModule({
   imports: [NgCommonModule, AngularSvgIconModule],
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, SelectorComponent],
   exports: [ButtonComponent]
 })
 export class CommonModule {}
