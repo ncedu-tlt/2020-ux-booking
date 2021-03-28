529cd274a45a95235bcc1dbf90b6751cde84eb95
NataliaKly <nataleeya77@gmail.com>
2021-03-28 23:32:02 +0400

    Notification component Storybook: controls, actions Some styles

---
+++ apps/frontend/src/app/common/notification/notification.component.ts
@@ -0,0 +1,42 @@
+import { Component, EventEmitter, HostBinding, Input, OnInit, Output } from '@angular/core';
+import { NotificationTypesEnum } from '../../enums/notification-types.enum';
+
+@Component({
+  selector: 'b-notification',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.less']
+})
+export class NotificationComponent implements OnInit {
+  @HostBinding('class') hostClass = 'notification';
+
+  @Input()
+  set type(value: NotificationTypesEnum) {
+    this._type = value;
+    this.setIcon();
+  }
+
+  @Input()
+  message: string;
+
+  @Output()
+  closeEvent: EventEmitter<void> = new EventEmitter<void>();
+
+  iconPath: string;
+  _type: string;
+
+  ngOnInit() {
+    this.setIcon();
+  }
+
+  private setIcon(): void {
+    if (this._type === NotificationTypesEnum.error) {
+      this.iconPath = 'assets/icons/cross.svg';
+    } else {
+      this.iconPath = 'assets/icons/checkmark.svg';
+    }
+  }
+
+  closeNotification(): void {
+    this.closeEvent.next();
+  }
+}
