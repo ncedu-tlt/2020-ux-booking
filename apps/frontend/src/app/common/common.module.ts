37129845569f4361a24865cdcf8f224b3fb95254
Natalia <36840773+NataliaKly@users.noreply.github.com>
2021-03-29 15:22:02 +0400

    Notification component (#15)
    * Notification component
    Storybook: controls, actions
    Some styles
    
    * [fix]
    Notification component
    Storybook: controls, actions
    Some styles

--- apps/frontend/src/app/common/common.module.ts
+++ apps/frontend/src/app/common/common.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule as NgCommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   imports: [NgCommonModule, AngularSvgIconModule],
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, NotificationComponent],
   exports: [ButtonComponent]
 })
 export class CommonModule {}
