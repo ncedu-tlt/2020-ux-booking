449cd733e8c0e2a35d7c68c32b23e8723adf3dc5
dimakolbasin <dimankolbasin@yandex.ru>
2021-04-06 10:29:39 +0400

    add header

--- apps/frontend/src/app/common/common.module.ts
+++ apps/frontend/src/app/common/common.module.ts
@@ -5,6 +5,7 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
 import { StepperComponent } from './stepper/stepper.component';
 import { NotificationComponent } from './notification/notification.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [NgCommonModule, AngularSvgIconModule],
@@ -12,13 +13,15 @@ import { DropdownComponent } from './dropdown/dropdown.component';
     ButtonComponent,
     StepperComponent,
     NotificationComponent,
-    DropdownComponent
+    DropdownComponent,
+    HeaderComponent
   ],
   exports: [
     ButtonComponent,
     StepperComponent,
     NotificationComponent,
-    DropdownComponent
+    DropdownComponent,
+    HeaderComponent
   ]
 })
 export class CommonModule {}
