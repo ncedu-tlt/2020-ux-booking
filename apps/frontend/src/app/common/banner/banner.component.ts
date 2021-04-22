a8d3536d37553fb11d8a73be03cd5982c5ceb259
dimakolbasin <dimankolbasin@yandex.ru>
2021-04-22 11:33:06 +0400

    fix css & html banner

--- apps/frontend/src/app/common/banner/banner.component.ts
+++ apps/frontend/src/app/common/banner/banner.component.ts
@@ -14,6 +14,8 @@ import {
 export class BannerComponent implements AfterViewInit {
   @ViewChild('description') el: ElementRef;
 
+  @ViewChild('scroll') ele: ElementRef;
+
   @Input()
   hotelInfo;
 
@@ -49,29 +51,25 @@ export class BannerComponent implements AfterViewInit {
     27
   ];
 
-  /*this.el.nativeElement.innerText = '123123123';*/
-
-  /*let descriptionHotel1 = this.hotelInfo.description.slice(0, -1);
-
-  this.descriptionHotel = descriptionHotel1;*/
-
   ngAfterViewInit() {
     this.blockReSizing();
   }
 
   public blockReSizing(): void {
-    while (this.el.nativeElement.offsetHeight > 74) {
+    while (this.el.nativeElement.offsetHeight > 60) {
       this.el.nativeElement.innerText = this.el.nativeElement.innerText.slice(
         0,
         -1
       );
-      /*const description = this.el.nativeElement.textContent;*/
+      this.hotelInfo.description = this.el.nativeElement.textContent;
     }
-    /*this.el.nativeElement.innerText = description + '...';*/
   }
 
-  /*onResize(): void {
-    this.blockReSizing();
-    console.log(this.el.nativeElement.textContent);
-  }*/
+  scrollLeft() {
+    this.ele.nativeElement.scrollLeft += 200;
+  }
+
+  scrollRight() {
+    this.ele.nativeElement.scrollLeft -= 200;
+  }
 }
