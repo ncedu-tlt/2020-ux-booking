1c86098361b8463034ed1e9f792905cc12b35be3
dimakolbasin <dimankolbasin@yandex.ru>
2021-04-28 11:02:10 +0400

    fix css & html banner

--- apps/frontend/src/app/services/hotel-data.service.ts
+++ apps/frontend/src/app/services/hotel-data.service.ts
@@ -1,118 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { HotelInfoModel } from '../models/hotel-Info.model';
 
 @Injectable()
 export class HotelDataService {
-  hotelsInfo: HotelInfoModel[] = [
-    {
-      name: 'Hotel super puper ',
-      hotelImgUrl: 'assets/icons/hotel.jpg',
-      description:
-        'первый, eos elmet, consectetur adipisicing eos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuselit. Adipisci animi commodi cum debitis ' +
-        'delectus dolore doloremque, eos e',
-      address: {
-        country: 'russia',
-        city: 'moscow'
-      },
-      starsCount: 1,
-      countReviews: 10,
-      hotelRating: 9.6,
-      minPrice: 2300,
-      currency: '$',
-      freeCancellation: true,
-      services: [
-        {
-          name: 'car',
-          iconUrl: 'assets/icons/car.svg'
-        },
-        {
-          name: 'dryer',
-          iconUrl: 'assets/icons/dryer.svg'
-        }
-      ]
-    },
-    {
-      name: 'Hotel super puper ',
-      hotelImgUrl: 'assets/icons/hotel1.jpg',
-      description:
-        'второй, eos elmet, consectetur adipisicing eos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuselit. Adipisci animi commodi cum debitis ' +
-        'delectus dolore doloremque, eos e',
-      address: {
-        country: 'russia',
-        city: 'moscow'
-      },
-      starsCount: 1,
-      countReviews: 10,
-      hotelRating: 9.6,
-      minPrice: 2300,
-      currency: '$',
-      freeCancellation: true,
-      services: [
-        {
-          name: 'car',
-          iconUrl: 'assets/icons/car.svg'
-        },
-        {
-          name: 'dryer',
-          iconUrl: 'assets/icons/dryer.svg'
-        }
-      ]
-    },
-    {
-      name: 'Hotel super puper ',
-      hotelImgUrl: 'assets/icons/hotel2.jpg',
-      description:
-        'третий, eos elmet, consectetur adipisicing eos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuseos elmet, consectetur adipisicing elit. ' +
-        'Adipisci animi commodi cum debitis delectuselit. Adipisci animi commodi cum debitis ' +
-        'delectus dolore doloremque, eos e',
-      address: {
-        country: 'russia',
-        city: 'moscow'
-      },
-      starsCount: 1,
-      countReviews: 10,
-      hotelRating: 9.6,
-      minPrice: 2300,
-      currency: '$',
-      freeCancellation: true,
-      services: [
-        {
-          name: 'car',
-          iconUrl: 'assets/icons/car.svg'
-        },
-        {
-          name: 'dryer',
-          iconUrl: 'assets/icons/dryer.svg'
-        }
-      ]
-    }
-  ];
+  constructor(private http: HttpClient) {}
 
   getHotels(): Observable<HotelInfoModel[]> {
-    return of(this.hotelsInfo);
+    return this.http.get<HotelInfoModel[]>('/api/hotels');
   }
 }
