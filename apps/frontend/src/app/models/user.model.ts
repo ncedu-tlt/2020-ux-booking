ecadf742758feb1a5579b5f9542ce24dc6c2b7cd
Marjona Mirzoeva <mirzoeva.top@mail.ru>
2021-05-28 13:10:01 +0400

    User profile (frontend)

---
+++ apps/frontend/src/app/models/user.model.ts
@@ -0,0 +1,15 @@
+export type UserModel = {
+  firstName: string;
+
+  lastName: string;
+
+  patronymic: number;
+
+  phone: string;
+
+  dateOfBirth: string;
+
+  email: string;
+
+  emailNotification: boolean;
+};
