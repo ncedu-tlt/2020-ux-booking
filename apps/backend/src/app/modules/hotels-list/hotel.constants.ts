599f0c34623166cb134e6cd91fdd36b734127220
dimakolbasin <dimankolbasin@yandex.ru>
2021-06-03 12:59:21 +0400

    add hotel-controller, add hotel-service

---
+++ apps/backend/src/app/modules/hotels-list/hotel.constants.ts
@@ -0,0 +1,15 @@
+import { HotelRelations } from './hotel.relations';
+
+export const RELATIONS_GET_HOTEL_ID: string[] = [
+  HotelRelations.address,
+  HotelRelations.city,
+  HotelRelations.country,
+  HotelRelations.services,
+  HotelRelations.category,
+  HotelRelations.serviceType,
+  HotelRelations.currency,
+  HotelRelations.mainPhoto,
+  HotelRelations.photos,
+  HotelRelations.distance,
+  HotelRelations.hotelBoardBasis,
+  HotelRelations.boardBasis]
---
+++ apps/backend/src/app/modules/hotels-list/hotel.relations.ts
@@ -0,0 +1,14 @@
+export enum HotelRelations {
+  address = 'address',
+  city = 'address.city',
+  country = 'address.city.country',
+  services = 'services',
+  category = 'services.category',
+  serviceType = 'serviceType',
+  currency = 'currency',
+  mainPhoto = 'mainPhoto',
+  photos = 'photos',
+  distance = 'distance',
+  hotelBoardBasis = 'hotelBoardBasis',
+  boardBasis = 'hotelBoardBasis.boardBasis'
+}
