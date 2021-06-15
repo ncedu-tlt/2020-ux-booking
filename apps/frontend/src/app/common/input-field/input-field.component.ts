88bc3b78700d08a64561f933a826c5f0891d8e4d
rich-alien <Richalien@yandex.ru>
2021-06-15 14:15:32 +0300

    the intuit has been made more mobile

--- apps/frontend/src/app/common/input-field/input-field.component.ts
+++ apps/frontend/src/app/common/input-field/input-field.component.ts
@@ -1,20 +1,34 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  EventEmitter,
+  forwardRef,
   Input,
-  OnInit,
-  Output
+  OnInit
 } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validator
+} from '@angular/forms';
 
 @Component({
   selector: 'b-input-field',
   templateUrl: './input-field.component.html',
   styleUrls: ['./input-field.component.less'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputFieldComponent),
+      multi: true
+    }
+  ]
 })
-export class InputFieldComponent implements OnInit {
+export class InputFieldComponent
+  implements OnInit, ControlValueAccessor, Validator {
   @Input()
   title: string;
   @Input()
@@ -24,18 +38,53 @@ export class InputFieldComponent implements OnInit {
   @Input()
   isDisabled: boolean;
 
-  @Output() handleChange: EventEmitter<string> = new EventEmitter();
+  touched = false;
+  disabled = false;
+  input: FormControl = new FormControl();
+  onChange;
+  onTouched = () => {
+    //
+  };
 
-  _value = '';
-  input: FormControl;
+  validate(control: AbstractControl): ValidationErrors | null {
+    const quantity = control.value;
+    console.log(quantity);
+    if (quantity <= 0) {
+      return {
+        mustBePositive: {
+          quantity
+        }
+      };
+    }
+  }
+
+  writeValue(value: string): void {
+    this.input.setValue(value);
+  }
+
+  registerOnChange(onChange: any): void {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any): void {
+    this.onTouched = onTouched;
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
 
   ngOnInit(): void {
-    this.input = new FormControl(this._value, Validators.required);
     if (this.isDisabled) {
       this.input.disable();
     }
-    this.input.valueChanges.subscribe((value: string) => {
-      this.handleChange.emit(value);
+    this.input.valueChanges.subscribe((val: string) => {
+      if (this.onChange) {
+        this.onChange(val);
+      }
     });
   }
 }
